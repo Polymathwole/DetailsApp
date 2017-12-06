33b18ff3b27c390b7381a23450c54f217f8bb87b
Oluwaremi Oluwole <polymathwole@gmail.com>
2017-12-06 20:46:03 +0100

    Add appsettings to gitignore

--- DetailsApp/Startup.cs
+++ DetailsApp/Startup.cs
@@ -2,14 +2,19 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using DetailsApp.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace DetailsApp
 {
     public class Startup
     {
+
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(Configuration.))
             services.AddMvc();
         }
 
