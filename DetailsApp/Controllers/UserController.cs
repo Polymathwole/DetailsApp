2e71e874ab759050311e0d77d881542691a03b65
Oluwaremi Oluwole <polymathwole@gmail.com>
2017-12-08 10:06:46 +0100

    Add login/signup pages

--- DetailsApp/Controllers/UserController.cs
+++ DetailsApp/Controllers/UserController.cs
@@ -14,14 +14,47 @@ namespace DetailsApp.Controllers
             users = use;
         }
 
-        public async Task<ViewResult> GetOne() => View(await users.Users());
+        public ViewResult Index()
+        {
+            return View();
+        }
 
-        public async Task<ViewResult> Index(int? id)
+        [HttpGet]
+        public ViewResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Signup(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Dashboard");
+            }
+
+            return View();
+        }
+
+        public async Task<ViewResult> GetAll()
+        {
+            ViewBag.Title = "List Users";
+            return View("GetUsers", await users.Users());
+        }
+            
+
+        public async Task<ViewResult> GetOne(int? id)
         {
             if (id == null)
-                return View(null);
+            {
+                ViewBag.Title = "Get User";
+                return View("GetUsers", null);
+            }
             else
-                return View(await users.Find(id));
+            {
+                ViewBag.Title = "Get User";
+                return View("GetUsers", await users.Find(id));
+            }
         }
     }
 }
--- DetailsApp/Models/User.cs
+++ DetailsApp/Models/User.cs
@@ -1,19 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace DetailsApp.Models
 {
     public class User
     {
         public int UserID { get; set; }
+
+        [Required(ErrorMessage = "Specify title")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Specify first name")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Specify last name")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Specify gender")]
         public string Gender { get; set; }
+
+        [UIHint("Date")]
+        [Required(ErrorMessage = "Specify date of birth")]
         public DateTime? DoB { get; set; }
+
+        [StringLength(12,MinimumLength =6,ErrorMessage ="6 <= Username <= 12 characters")]
+        [Required(ErrorMessage = "Specify username")]
         public string Username { get; set; }
-        public string PasswordHash { get; set; }
+
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "8 <= Password <= 16 characters")]
+        [Required(ErrorMessage = "Specify password")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password and confirm does not match")]
+        [Required(ErrorMessage = "Confirm password")]
+        public string PasswordConf { get; set; }
     }
 }
